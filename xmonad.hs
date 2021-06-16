0dbd47799ca8995c0c074f4d66fa1580719c1bda
Sravan Balaji <balajsra@umich.edu>
2021-06-15 21:01:40 -0400

    Fullscreen Fixes
    - Add fullscreen event hook
    - Whitespace changes
    - Add some comments

--- xmonad.hs
+++ xmonad.hs
@@ -161,17 +161,20 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     ]
 
 myLayout =
-  avoidStruts ( tiled ||| grid ||| monocle )
+  -- remove borders on floating windows that cover the whole screen
+  lessBorders OnlyScreenFloat
+  -- avoid struts (status bar)
+  $ avoidStruts ( tiled ||| grid ||| monocle )
   where
      -- Gaps around and between windows
      -- Changes only seem to apply if I log out then in again
      -- Dimensions are given as (Border top bottom right left)
-     mySpacing = spacingRaw False               -- Only for >1 window
+     mySpacing = spacingRaw False                -- Only for >1 window
                             -- The bottom edge seems to look narrower than it is
                             (Border 15 15 15 15) -- Size of screen edge gaps
-                            True                -- Enable screen edge gaps
-                            (Border 10 10 10 10)    -- Size of window gaps
-                            True                -- Enable window gaps
+                            True                 -- Enable screen edge gaps
+                            (Border 10 10 10 10) -- Size of window gaps
+                            True                 -- Enable window gaps
 
      -- default tiling algorithm partitions the screen into two panes
      nmaster = 1
@@ -198,7 +201,7 @@ myManageHook = composeAll
 
 myPlacement = withGaps (16,0,16,0) (smart (0.5,0.5))
 
-myEventHook = ewmhDesktopsEventHook
+myEventHook = ewmhDesktopsEventHook <+> fullscreenEventHook
 
 myStartupHook = return()
 
